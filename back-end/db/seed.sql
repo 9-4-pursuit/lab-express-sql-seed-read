c9297099e1999036b5456151ad27275064fada43
SuperNinjaEv <evan0210@gmail.com>
2023-04-25 18:20:32 -0400

    Modified seed data

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,8 +1,8 @@
 \c songs_dev
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
-('5D', 'Qveen Herby', 'The Muse', '2 minutes 39 seconds', true),
-('Somewhere in the Between', 'Streetlight Manifesto', 'Somewhere in the Between', '3 minutes 43 seconds', true),
-('Need to Know', 'Doja Cat', 'Planet Her', '3 minutes 30 seconds', false),
-('Guerilla Radio', 'Rage Against The Machine', 'The Battle of Los Angeles', '3 minutes 26 seconds', false),
-('Ocarina of Time: Main Theme', 'Koji Kondo', 'The Legend of Zelda: Ocarina of Time', '1 minute 22 seconds', true);
+('5D', 'Qveen Herby', 'The Muse', '2:39', true),
+('Somewhere in the Between', 'Streetlight Manifesto', 'Somewhere in the Between', '3:43', true),
+('Need to Know', 'Doja Cat', 'Planet Her', '3:30', false),
+('Guerilla Radio', 'Rage Against The Machine', 'The Battle of Los Angeles', '3:26', false),
+('Ocarina of Time: Main Theme', 'Koji Kondo', 'The Legend of Zelda: Ocarina of Time', '1 minute 22', true);
