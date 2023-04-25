976f37fa5fdbff030e8db0989669739bbb1bf36d
arerimr <raydelys.morrobelr@gmail.com>
2023-04-24 22:07:12 -0400

    returning all songs

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const song = express.Router()
+const { getAllSongs } = require('../queries/songs.js')
 
 song.get('/', async (req, res) => {
-    res.status(200).send('the index')
+    const allSongs = await getAllSongs();
+    allSongs ? res.status(200).json(allSongs) : res.status(500).json({ error: 'server error' })
 });
 
 module.exports = song;
