027a77f42fd755e309b9115d9a2eac075d3bda82
BertCasale <bthebest55@gmail.com>
2023-04-25 14:50:46 -0400

    fix seed.sql errors

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,6 +1,6 @@
 \c tuner
 
-INSERT INTO songs (name, artist, album, time, is_favorite) VALUE 
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
 ('THATS WHAT I WANT', 'Lil Nas X', 'MONTERO', '2:23', true), 
 ('Just One Yesterday', 'Fall Out Boy', 'Save Rock and Roll', '4:04', true), 
 ('Circles', 'Post Malone', 'Hollywood''s Bleeding', '3:35', false),
@@ -12,4 +12,4 @@ INSERT INTO songs (name, artist, album, time, is_favorite) VALUE
 ('Old Town Road', 'Lil Nas X', '7', '1:53', false),
 ('Good Day', 'Twenty One Pilots', 'Scaled and Icy', '3:24', false),
 ('Thnks fr th Mmrs', 'Fall Out Boy', 'Infinity on High', '3:23', true), 
-('Diamonds', 'Sam Smith', 'Love Goes', '3:32', false)
+('Diamonds', 'Sam Smith', 'Love Goes', '3:32', false);
