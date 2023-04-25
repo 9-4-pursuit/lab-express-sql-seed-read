f4f16be466e545d380620c0ded010b85b78f8deb
arerimr <raydelys.morrobelr@gmail.com>
2023-04-24 21:58:29 -0400

    inserted data into table

---
+++ back-end/db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('if', 'davido', 'idk', '8:00', false),
+('wake up', 'chinx', 'idk', '9:00', true),
+('Yandel 150', 'yandel', 'idk', '10:00', false);
