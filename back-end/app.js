825b522f65ce0f6a30c436b182c3384fc889c5f1
SuperNinjaEv <evan0210@gmail.com>
2023-04-25 18:12:37 -0400

    Schema and Seed created, database live and functioning

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const songsController = require("./controllers/songController");
+const songsController = require("./controllers/songController");
 
 app.use(express.json());
 app.use(cors());
 
-// app.use("/songs", songsController);
+app.use("/songs", songsController);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Songs Back-End");
