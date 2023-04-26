b50560d7279d915d9bf510b4a3bc794a553ccb6a
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-04-25 21:17:16 -0400

    server & postman is functional all CRUD complete

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -9,9 +9,11 @@ CREATE DATABASE songs_dev;
 
 -- Create a table for our songs
 CREATE TABLE songs (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    url TEXT,
-    category TEXT,
-    is_favorite BOOLEAN
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  artist TEXT,
+  album TEXT,
+  time TEXT,
+  category TEXT,
+  is_favorite BOOLEAN
 );
