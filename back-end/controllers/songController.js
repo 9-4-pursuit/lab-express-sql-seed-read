55b2ed3721d08d054b360b04660554fcdea27627
SuperNinjaEv <evan0210@gmail.com>
2023-04-25 22:05:34 -0400

    Completed Create & Delete functionality

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const song = express.Router();
-const { getAllSongs, getASong } = require("../queries/songs");
+const { getAllSongs, getASong, createSong, deleteSong } = require("../queries/songs");
 
 // GET ALL
 song.get("/", async (req, res) => {
@@ -14,7 +14,7 @@ song.get("/", async (req, res) => {
     }
 });
 
-// INDIVIDUAL
+// SHOW ONE
 song.get("/:id", async (req, res) => {
     const { id } = req.params;
     const song = await getASong(id);
@@ -23,6 +23,30 @@ song.get("/:id", async (req, res) => {
         res.status(202).json(song);
     } else {
         res.status(500).json({ error: "Server Error Show" });
+    }
+});
+
+// CREATE ROUTE
+song.post("/", async (req, res) => {
+    const newSong = req.body;
+
+    try {
+        const addedSong = await createSong(newSong);
+        res.status(202).json(addedSong);
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+});
+
+// DELETE ROUTE
+song.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedSong = await deleteSong(id);
+        res.status(200).json(deletedSong);
+    } catch (error) {
+        res.status(400).json({ error: error })
     }
 });
 
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -18,8 +18,31 @@ const getASong = async (id) => {
         return error;
     }
 };
+const createSong = async (songToAdd) => {
+    const { name, artist, album, time, is_favorite } = songToAdd;
+
+    try {
+        const newSong = await db.one
+            ("INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+                [name, artist, album, time, is_favorite]);
+        return newSong;
+    } catch (error) {
+        return error;
+    }
+};
+
+const deleteSong = async (id) => {
+    try {
+        const deletedSong = await db.one("DELETE FROM songs WHERE id=$1 RETURNING *", id)
+        return deletedSong;
+    } catch (error) {
+        return error;
+    }
+};
 
 module.exports = {
     getAllSongs,
-    getASong
+    getASong,
+    createSong,
+    deleteSong
 };
