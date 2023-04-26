cfcae550507b53095475d2a33d7a0ebfdb3bee38
pjungjs <pjungjs@gmail.com>
2023-04-25 22:02:13 -0400

    part 2 and 3 done. setup update, create and delete routes.

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const songs = express.Router();
-const { getAllSongs, getOneSong } = require('../queries/songs.js');
+const { getAllSongs, getOneSong, editSong, createSong, deleteSong } = require('../queries/songs.js');
 
+//index route
 songs.get('/', async (req, res) => {
   const allSongs = await getAllSongs();
 
@@ -12,6 +13,7 @@ songs.get('/', async (req, res) => {
   }
 })
 
+//show route
 songs.get('/:id', async (req, res) => {
   const { id } = req.params;
   const song = await getOneSong(id);
@@ -22,5 +24,43 @@ songs.get('/:id', async (req, res) => {
     res.status(500).json({ error: 'Server Error.'});
   }
 })
+
+//update route
+songs.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const updateSong = req.body;
+
+  try {
+    const updatedSong = await editSong(id, updateSong);
+    res.status(202).json(updatedSong);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+})
+
+//create route
+songs.post('/', async (req, res) => {
+  const newSong = req.body;
+
+  try {
+    const addedSong = await createSong(newSong);
+    res.status(202).json(addedSong);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+})
+
+//delete route
+songs.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedSong = await deleteSong(id);
+    res.status(200).json(deletedSong);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+})
+
 
 module.exports = songs;
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -1,5 +1,6 @@
 const db = require('../db/dbConfig.js');
 
+//index query
 const getAllSongs = async () => {
   try {
     const allSongs = await db.any("SELECT * FROM songs;");
@@ -9,6 +10,7 @@ const getAllSongs = async () => {
   }
 }
 
+//show query
 const getOneSong = async (id) => {
   try {
     const song = await db.one("SELECT * FROM songs WHERE id=$1;", id);
@@ -18,7 +20,45 @@ const getOneSong = async (id) => {
   }
 }
 
+//edit query
+const editSong = async (id, songToEdit) => {
+  const { name, artist, album, time, is_favorite } = songToEdit;
+
+  try {
+    const editedSong = await db.one("UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 WHERE id=$6 RETURNING *;", [name, artist, album, time, is_favorite, id]);
+    return editedSong;
+  } catch (error) {
+    return error;
+  }
+}
+
+//create query
+const createSong = async (songToAdd) => {
+  const { name, artist, album, time, is_favorite } = songToAdd;
+
+  try {
+    const newSong = await db.one("INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [name, artist, album, time, is_favorite]);
+    return newSong;
+  } catch (error) {
+    return error;
+  }
+}
+
+//delete query
+const deleteSong = async (id) => {
+  try {
+    const deletedBookmark = await db.one("DELETE FROM songs WHERE id=$1 RETURNING *;", id);
+    return deletedBookmark;
+  } catch (error) {
+    return error;
+  }
+}
+
+
 module.exports = {
   getAllSongs,
-  getOneSong
+  getOneSong,
+  editSong,
+  createSong,
+  deleteSong
 };
