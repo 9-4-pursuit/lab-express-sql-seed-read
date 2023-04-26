639120dd03c0b2779ddc23edd28538a2d99b4877
arerimr <raydelys.morrobelr@gmail.com>
2023-04-25 20:30:39 -0400

    create new song

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const song = express.Router()
-const { getAllSongs, getOneSong } = require('../queries/songs.js')
+const { getAllSongs, getOneSong, createSong } = require('../queries/songs.js')
 
 song.get('/', async (req, res) => {
     const allSongs = await getAllSongs();
@@ -11,6 +11,12 @@ song.get('/:id', async (req, res) => {
     const { id } = req.params
     const song = await getOneSong(id)
     song ? res.status(200).json(song) : res.status(500).json({ error: 'server error' })
+});
+
+song.post('/', async (req, res) => {
+    const newSong = req.body
+    const createdSong = await createSong(newSong)
+    createdSong ? res.status(200).json(createdSong) : res.status(500).json({ error: 'server error' })
 });
 
 module.exports = song;
