63df822b405bb9191a08614e46ed210a1001ae2e
BertCasale <bthebest55@gmail.com>
2023-04-25 20:32:20 -0400

    create a song

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const {getAllSongs, getOneSong} = require("../queries/songs.js");
+const {getAllSongs, getOneSong, createSong} = require("../queries/songs.js");
 
 songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
@@ -21,8 +21,18 @@ songs.get("/:id", async (req, res) => {
   } else {
     res.status(500).json({error: "Server Error"});
   }
+});
 
-  
-})
+songs.post("/", async (req, res) => {
+  const newSong = req.body;
+
+  try {
+    const addedSong = await createSong(newSong);
+
+    res.status(202).json(addedSong);
+  } catch (error) {
+    res.status(400).json({error: error});
+  }
+});
 
 module.exports = songs;
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -18,7 +18,20 @@ const getOneSong = async (id) => {
   }
 }
 
+const createSong = async (songToAdd) => {
+  const {name, artist, album, time, is_favorite} = songToAdd;
+
+  try {
+    const newSong = await db.one("INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *", [name, artist, album, time, is_favorite]);
+    return newSong;
+  } catch (error) {
+    return error;
+  }
+}
+
+
 module.exports = {
   getAllSongs,
-  getOneSong
+  getOneSong,
+  createSong
 }
