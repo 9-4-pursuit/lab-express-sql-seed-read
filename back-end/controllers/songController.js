bfb26891db522692920bd1a736c8ea018c343eef
arerimr <raydelys.morrobelr@gmail.com>
2023-04-25 20:47:09 -0400

    delete song

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const song = express.Router()
-const { getAllSongs, getOneSong, createSong } = require('../queries/songs.js')
+const { getAllSongs, getOneSong, createSong, deleteSong } = require('../queries/songs.js')
 
 song.get('/', async (req, res) => {
     const allSongs = await getAllSongs();
@@ -17,6 +17,12 @@ song.post('/', async (req, res) => {
     const newSong = req.body
     const createdSong = await createSong(newSong)
     createdSong ? res.status(200).json(createdSong) : res.status(500).json({ error: 'server error' })
+});
+
+song.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    const deletedSong = await deleteSong(id)
+    deletedSong ? res.status(200).json(deletedSong) : res.status(500).json({ error: 'server error' })
 });
 
 module.exports = song;
