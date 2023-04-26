e11672305e5b49cc10599ce62baae6d5e3f65ef2
arerimr <raydelys.morrobelr@gmail.com>
2023-04-25 20:15:26 -0400

    get one song route

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const song = express.Router()
-const { getAllSongs } = require('../queries/songs.js')
+const { getAllSongs, getOneSong } = require('../queries/songs.js')
 
 song.get('/', async (req, res) => {
     const allSongs = await getAllSongs();
     allSongs ? res.status(200).json(allSongs) : res.status(500).json({ error: 'server error' })
+});
+
+song.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const song = await getOneSong(id)
+    song ? res.status(200).json(song) : res.status(500).json({ error: 'server error' })
 });
 
 module.exports = song;
