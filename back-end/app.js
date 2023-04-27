0f1538ecde3d8561ad53b85c0b001a9bd01f6487
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-04-26 21:07:47 -0400

    passed first test

--- back-end/app.js
+++ back-end/app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use("/songs", songsController)
 
 app.get("/", (req, res) => {
-   res.send("Welcome to the Songs App") 
+   res.send("Welcome to Tuner") 
 })
 
 app.get("*", (req, res) => {
