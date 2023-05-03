62063a3c53a450606c88a0ef8d379cab67488ce4
BertCasale <bthebest55@gmail.com>
2023-05-03 19:13:53 -0400

    display single song details

--- front-end/src/Components/SongDetails.js
+++ front-end/src/Components/SongDetails.js
@@ -1,5 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL;
+
 export default function SongDetails () {
+  const {id} = useParams();
+  const [song, setSong] = useState({});
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${API}/songs/${id}`)
+    .then((res) => setSong(res.data))
+    .catch((e) => console.warn("catch", e))
+  }, [id]);
+
   return (<div className="SongDetails">
-    Song Details
+    <h3>
+      <span className="favorite"> &nbsp; {song.is_favorite ? "⭐️" : "❌"} &nbsp; </span>
+      {song.name} - By {song.artist}
+    </h3>
+    <h4 className="album">{song.album}</h4>
+    <h5 className="duration">Duration: {song.time}</h5>
   </div>);
 }
