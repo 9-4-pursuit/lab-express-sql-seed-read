beb3e6bb2dab6a54e6276d273167388f6f3b3eb5
BertCasale <bthebest55@gmail.com>
2023-05-02 21:31:08 -0400

    song component maps through db songs

--- front-end/src/Components/Songs.js
+++ front-end/src/Components/Songs.js
@@ -1,5 +1,42 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+import Song from "./Song.js";
+
+const API = process.env.REACT_APP_API_URL;
+
 export default function Songs () {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API}/songs`)
+    .then((response) => 
+    {
+      setSongs(response.data);
+      console.log(response.data);
+    })
+    .catch((e) => console.warn("catch", e));
+  }, []);
+
   return (<div className="Songs">
-    Songs
+    <table>
+
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Favorite</th>
+          <th>Artist</th>
+          <th>Duration</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {songs.map((song) => {
+          return <Song 
+            key={song.id} 
+            song={song}/>
+        })}
+      </tbody>
+
+    </table>
   </div>);
 }
