2215972bd0728eb49f14bdb7320510b7b7f5db03
arerimr <raydelys.morrobelr@gmail.com>
2023-05-02 21:24:25 -0400

    set up songs

---
+++ front-end/src/Components/Song.js
@@ -0,0 +1,9 @@
+
+
+export default function Song(){
+    return(
+        <div className="Song">
+            <h1>Song</h1>
+        </div>
+    )
+}
--- front-end/src/Components/Songs.js
+++ front-end/src/Components/Songs.js
@@ -1,8 +1,20 @@
+import Song from "./Song";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const API = process.env.REACT_APP_API_URL;
 
-export default function Songs(){
-    return(
-        <div className="songs">
+export default function Songs() {
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${API}/songs`)
+            .then((res) => setSongs(res.data))
+            .catch((e) => console.warn(e))
+    })
+
+    return (
+        <div className="Songs">
             <h1>songs</h1>
         </div>
     )
