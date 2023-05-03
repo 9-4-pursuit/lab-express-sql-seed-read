13b2ee9cf602c8655ece9904cd18ad45f6fee5e7
BertCasale <bthebest55@gmail.com>
2023-05-02 22:06:11 -0400

    populate song data on songs index

--- front-end/src/Components/Song.js
+++ front-end/src/Components/Song.js
@@ -1,5 +1,12 @@
-export default function Song () {
-  return (<div className="Song">
-    Song
-  </div>);
+import {Link} from "react-router-dom";
+
+export default function Song ({song}) {
+  return (<tr className="Song">
+    <td className="title">
+      <Link to={`/songs/${song.id}`}>{song.name}</Link>
+    </td>
+    <td className="favorite">{song.is_favorite ? <span>⭐️</span> : <span>&nbsp; &nbsp; &nbsp;</span>}</td>
+    <td className="artist">{song.artist}</td>
+    <td className="duration">{song.time}</td>
+  </tr>);
 }
