ad8f8170bee350f1c102fdb2f2914cfb2197c0e7
MaxWattan <mcwattanac@gmail.com>
2023-05-02 21:48:54 -0400

    passed test

--- back-end/validations/checkSongs.js
+++ back-end/validations/checkSongs.js
@@ -1,5 +1,10 @@
 // Middleware
 const checkBoolean = (is_favorite) => {
+    if(typeof is_favorite === "boolean"){
+      return is_favorite  
+    }
+    
+
     if (is_favorite === "false") {
         return false
     } else if (is_favorite === "true"){
@@ -8,7 +13,8 @@ const checkBoolean = (is_favorite) => {
 }
 
 const checkRequest = (req, res, next) => {
-   const isFavorite= checkBoolean(req.body.is_favorite)
+
+   const is_favorite= checkBoolean(req.body.is_favorite)
 
     if (
         req.body.time && 
