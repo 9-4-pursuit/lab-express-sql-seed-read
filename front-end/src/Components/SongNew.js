60731261cb587851dd26ca04e04238e221da6b2e
BertCasale <bthebest55@gmail.com>
2023-05-03 21:29:31 -0400

    create a new song

--- front-end/src/Components/SongNew.js
+++ front-end/src/Components/SongNew.js
@@ -1,38 +1,86 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL;
+
 export default function SongNew () {
-  
+  let navigate = useNavigate();
+  const [song, setSong] = useState({
+    name: "",
+    artist: "",
+    album: "",
+    time: "",
+    is_favorite: false
+  });
+
+  const handleTextChange = (event) => {
+    setSong({...song, [event.target.id]: event.target.value});
+  }
+
+  const addSong = (newSong) => {
+    axios.post(`${API}/songs`, newSong)
+    .then(() => {
+      navigate("/songs")
+    },
+      (err) => console.error(err)
+    )
+    .catch((e) => console.warn("catch", e));
+  }
+
+  const handleCheckboxChange = () => {
+    setSong({...song, is_favorite: !song.is_favorite});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addSong(song);
+  }
+
   return (<div className="SongNew">
-    <form>
+    <form onSubmit={handleSubmit}>
 
       <label htmlFor="name">Song Name:</label>
       <input 
         type="text"
         id="name"
-        name="name"></input>
+        name="name"
+        value={song.name}
+        onChange={handleTextChange}
+        required/>
 
       <label htmlFor="artist">Artist:</label>
       <input 
         type="text"
         id="artist"
-        name="artist"></input>
+        name="artist"
+        value={song.artist}
+        onChange={handleTextChange}
+        required/>
 
       <label htmlFor="album">Album:</label>
       <input
         type="text"
         id="album"
-        name="album"></input>
+        name="album"
+        value={song.album}
+        onChange={handleTextChange}/>
 
       <label htmlFor="time">Duration:</label>
       <input 
         type="text"
         id="time"
         name="time"
-        placeholder="00:00"></input>
+        value={song.time}
+        placeholder="00:00"
+        onChange={handleTextChange}/>
 
       <label htmlFor="is_favorite">Favorite:</label>
       <input
         type="checkbox"
         id="is_favorite"
-        name="is_favorite"/>   
+        name="is_favorite"
+        checked={song.is_favorite}
+        onChange={handleCheckboxChange}/>   
 
       <input type="submit"/>  
 
