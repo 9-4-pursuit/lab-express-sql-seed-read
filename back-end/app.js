abfcca08b6624d26f630819e54504a0178772331
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-04 19:34:14 -0400

    created update and delte routes in controller

--- back-end/app.js
+++ back-end/app.js
@@ -15,10 +15,10 @@ app.use("/songs", songController);
 //routes
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to turner");
+  res.status(200).send("Welcome to Tuner");
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send("PAGE NOT FOUND ");
+  res.status(400).send("PAGE NOT FOUND ");
 });
 module.exports = app;
--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,33 +1,64 @@
 const express = require("express");
 const songs = express.Router();
 
-const { getAllSongs, getASong, newSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getASong,
+  newSong,
+  updateSong,
+} = require("../queries/songs");
 
+//show
 songs.get("/", async (req, res) => {
   const allSongs = await getAllSongs();
   if (allSongs) {
-    res.status(202).json(allSongs);
+    res.status(200).json(allSongs);
   } else {
     res.status(500).json({ error: "Server error" });
   }
 });
-
+//index
 songs.get("/:id", async (req, res) => {
   const { id } = req.params;
   const aSong = await getASong(id);
   if (aSong) {
-    res.status(202).json(aSong);
+    res.status(200).json(aSong);
   } else {
     res.status(500).json({ error: error });
   }
 });
+//create
 songs.post("/", async (req, res) => {
   const lastestSong = req.body;
   const addedSong = await newSong(lastestSong);
   if (addedSong) {
-    res.status(202).json(addedSong);
+    res.status(200).json(addedSong);
   } else {
     res.status(500).json({ error: error });
   }
 });
+
+//delte
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  if (deletedSong) {
+    res.status(200).json(deletedSong);
+  } else {
+    res.res(500).json({ error: error });
+  }
+});
+
+//updated
+songs.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = req.body;
+  const updatedSong = await updateSong(id, song);
+  if (updatedSong) {
+    res.status(200).json(updateSong);
+  } else {
+    res.status(500).json({ error: "error" });
+  }
+});
+
 module.exports = songs;
