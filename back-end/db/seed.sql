9200959ae2a5b9beb995a536f3b037a67cd0f2b5
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-03 20:55:08 -0400

    seed.sql created

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,3 +1,13 @@
 \c songs_dev;
 
-INSERT (name,arist,album,time,is_favorite )
+INSERT INTO songs
+
+(name, artist, album, time, is_favorite ) 
+
+VALUES 
+
+('Stairway to Heaven', 'Led Zeppelin', 'Untitled Fourth Album', '08:03', true),
+('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', '05:55', true),
+('Imagine', 'John Lennon', 'Imagine', '03:03', false),
+('Like a Rolling Stone', 'Bob Dylan', 'Highway 61 Revisited', '06:13', true),
+('Purple Haze', 'Jimi Hendrix', 'Are You Experienced', '02:50', false);
