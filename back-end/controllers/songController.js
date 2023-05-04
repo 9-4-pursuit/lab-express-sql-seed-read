c46cdc196e70b0f32aa002d0791adc545adf3242
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-03 21:13:24 -0400

    controllers file intial route done

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,4 +1,15 @@
 const express = require("express");
-const songs = ex;
+const songs = express.Router();
 
 const getAllSongs = require("../queries/songs");
+
+songs.get("/", (req, res) => {
+  const allSongs = getAllSongs;
+  if (allSongs) {
+    res.status(202).json(allSongs);
+  } else {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+module.exports = songs;
