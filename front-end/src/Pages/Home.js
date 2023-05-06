3feed12b2092347072a8fa8e690502fd5ee510a3
aardvarkpepper <aardvarkpepper@gmail.com>
2023-05-06 12:56:17 -0400

    Add src>Pages>Home.js

---
+++ front-end/src/Pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h2>Tuner App</h2>
+        <p>A tuner converts received transmissions to a form suitable for further processing.  Such functionality is not actually directly related to the functionality of this app.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
