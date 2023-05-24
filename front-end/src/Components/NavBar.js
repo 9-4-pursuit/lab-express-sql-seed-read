de7cbaa44e009abcdbab6148a994c752046cf4e6
Diegocarrero <diegocar2413@gmail.com>
2023-05-24 16:09:20 -0400

    edit index page

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 export default function NavBar() {
     return (
         <nav className="Navbar">
+            <h1>Tuner App</h1>
             <h1>
                 <Link to="/songs">Songs</Link>
             </h1>
