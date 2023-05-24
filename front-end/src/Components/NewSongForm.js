d62ebcd04c1c619a7091efb7788bb52f138bcbf8
Diegocarrero <diegocar2413@gmail.com>
2023-05-24 16:17:47 -0400

    add state, fetch, handle change and handle submit functions to new page

--- front-end/src/Components/NewSongForm.js
+++ front-end/src/Components/NewSongForm.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-
+const API = process.env.REACT_APP_API_URL;
 
 export default function NewSongForm() {
+
+    const [song, setSong] = useState({
+        name: "",
+        artist: "",
+        album: "",
+        is_favorite: false,
+        time: ""
+    })
+
+    const navigate = useNavigate()
+
+    const handleTextChange = (e) => {
+        setSong({ ...song, [e.target.id]: e.target.value });
+    };
+
+    const handleCheckboxChange = () => {
+        setSong({ ...song, is_favorite: !song.is_favorite });
+    };
+
+    const addSong = (newSong) => {
+        axios.post(`${API}/songs`, newSong)
+            .then(() => navigate('/songs'))
+            .catch((e) => console.warn(e))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addSong(song)
+    }
+    
     return (
         <div className="New">
 
