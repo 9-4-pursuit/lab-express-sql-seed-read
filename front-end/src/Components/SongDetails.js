153fba532b66875379fec2a38be4e2d460645df1
Diegocarrero <diegocar2413@gmail.com>
2023-05-24 16:13:47 -0400

    add state, useeffect and delete function to show page

--- front-end/src/Components/SongDetails.js
+++ front-end/src/Components/SongDetails.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 
-
+const API = process.env.REACT_APP_API_URL;
 
 export default function SongDetails() {
+
+    const [song, setSong] = useState({})
+
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get(`${API}/songs/${id}`)
+            .then((res) => setSong(res.data))
+            .catch((e) => console.warn(e))
+    }, [id])
+
+    const handleDelete = () => {
+        axios.delete(`${API}/songs/${id}`)
+            .then(() => navigate('/songs'))
+    }
+
     return (
         <div className="Show">
 
