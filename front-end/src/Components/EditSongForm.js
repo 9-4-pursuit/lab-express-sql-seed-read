dde3508582c07185ceba9b82e8989fbba4b397ea
Diegocarrero <diegocar2413@gmail.com>
2023-05-24 16:22:29 -0400

    add content to edit page

--- front-end/src/Components/EditSongForm.js
+++ front-end/src/Components/EditSongForm.js
@@ -43,7 +43,52 @@ export default function EditSongForm() {
     }
     return (
         <div className="Edit">
-
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Song Name:</label>
+                <input
+                    id="name"
+                    value={song.name}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Name of song"
+                    required
+                />
+                <label htmlFor="artist">Artist:</label>
+                <input
+                    id="artist"
+                    value={song.artist}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Name of artist"
+                    required
+                />
+                <label htmlFor="album">Album:</label>
+                <input
+                    id="album"
+                    value={song.album}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Name of album"
+                    required
+                />
+                <label htmlFor="is_favorite">Favorite:</label>
+                <input
+                    id="is_favorite"
+                    type="checkbox"
+                    onChange={handleCheckboxChange}
+                    checked={song.is_favorite}
+                />
+                <label htmlFor="time">Time:</label>
+                <input
+                    id="time"
+                    value={song.time}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="0:00"
+                    required
+                />
+                <input type="submit" />
+            </form>
         </div>
     )
 }
